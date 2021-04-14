acd508fd065ae1c37e45d1328e48245cc675c9d8
r3n0us <slykourinos@evoxco.com>
2021-04-14 16:48:43 +0100

    TYS 0x01 WriteUp

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# tys
+## TYS - Test Your Skills 
+
+TYS is a series of challenges from Richard Sammet located at  https://mytty.org/ aiming to teach some security concepts in a variety of subjects. I have this repositry to save the solutions when needed.
+
+
+##### tys_0x01
+
+"Communication in the shadows"   
+```
+$ python3 tys_0x01_solution.py
+```
+```
+This is a secret message. Evesdropping is not allowed! Stay away!  
+The username is Hans and the password is Wurst
+You can use these credentials to login under https://www.facebook.com/
+Ah, jeez, what else... I figure I just add some more data in order to make it easier to identify the pattern
+```
---
+++ tys_0x01_solution.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env ptyhon
+from scapy.all import *
+pkts = rdpcap("tys_0x01_c2.pcapng")
+keys = ""
+encoded_text = ""
+
+
+def xor(key, enc_text):
+    cnt = 0
+    plain_text = ""
+    for c in range(0, len(enc_text), 1):
+        plain_text += chr( ord(key[cnt]) ^ ord(enc_text[c]) )
+        cnt += 1
+        if cnt == len(key):
+            cnt = 0
+    print(plain_text)
+
+
+for pkt in pkts:
+    if pkt.haslayer('Raw') and pkt.getlayer('Raw').load[0] == 0x02:
+        keys = pkt.getlayer('Raw').load[2:int(pkt.getlayer('Raw').load[1])+2]
+        keys = keys.decode('utf-8')
+    elif pkt.haslayer('Raw') and pkt.getlayer('Raw').load[0] == 0x04:
+        encoded_text = pkt.getlayer('Raw').load[2:int(pkt.getlayer('Raw').load[1])+2]
+        encoded_text = encoded_text.decode('utf-8')
+        xor(keys, encoded_text)
